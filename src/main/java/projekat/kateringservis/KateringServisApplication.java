7495c89d242c0cffa47c7b588f8500e12da42206
AleksaK00 <aleksa.karamarkovic@gmail.com>
2025-04-09 18:03:19 +0200

    Initial commit

---
+++ src/main/java/projekat/kateringservis/KateringServisApplication.java
@@ -0,0 +1,13 @@
+package projekat.kateringservis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KateringServisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KateringServisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projekat/kateringservis/KateringServisApplicationTests.java
@@ -0,0 +1,13 @@
+package projekat.kateringservis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KateringServisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
